2650e5042d5b9bc8065eccefc8359e8c600ab4ec
Mgonand <marcos@binpar.com>
2017-08-31 22:04:24 +0200

    getMongooseField

--- Bi-Model/__tests__/mongooseModel.js
+++ Bi-Model/__tests__/mongooseModel.js
@@ -2,13 +2,14 @@ import fullSampleModel from '../sample/full/model';
 import getMongooseField from '../src/parser/getMongooseField';
 
 const { describe, test, expect } = global;
+const Customers = fullSampleModel.find(entity => entity.shortName === 'Customers');
 
 describe('Generation of mongoose model', () => {
   test('Get mongoose field from entity model', async () => {
     expect(
       await getMongooseField(
         'phone',
-        fullSampleModel.find(entity => entity.shortName === 'Customers').model.phone,
+        Customers.model.phone,
       ),
     ).toMatchObject({
       type: String,
@@ -16,10 +17,20 @@ describe('Generation of mongoose model', () => {
     expect(
       await getMongooseField(
         'firstContactAt',
-        fullSampleModel.find(entity => entity.shortName === 'Customers').model.firstContactAt,
+        Customers.model.firstContactAt,
       ),
     ).toMatchObject({
       type: Date,
+      index: true,
+    });
+    expect(
+      await getMongooseField(
+        'lastName',
+        Customers.model.lastName,
+      ),
+    ).toMatchObject({
+      type: String,
+      required: true,
       index: true,
     });
   });
